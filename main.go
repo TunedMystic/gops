d5a5454189c2e9feeccc674c377727a00fd6b023
Sandeep Jadoonanan <jsanweb@gmail.com>
2017-10-01 15:03:18 -0400

    Print length of args

--- main.go
+++ main.go
@@ -52,7 +52,7 @@ func main() {
 
 	// Print args or defaults.
 	if len(args) > 0 {
-		fmt.Println(strings.Join(args, " "))
+		fmt.Printf("(%d) %v\n", len(args), strings.Join(args, " "))
 	} else {
 		printInfo()
 	}
